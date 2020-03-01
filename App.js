28a146c9d6240731c34cf4014d4d12397f6d9061
sakshid07 <53994005+sakshid07@users.noreply.github.com>
2020-03-01 13:22:40 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { BrowserRouter as Router , Route, NavLink } from 'react-router-dom';
+import SignUpForm from "./pages/SignUpForm";
+import SignInForm from "./pages/SignInForm";
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+      <div className="App__Aside"></div>
+      <div className="App__Form">
+      <div className="PageSwitcher">
+      <NavLink to="/sign-in" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign In</NavLink>
+      <NavLink exact to="/" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign Up</NavLink>
+    </div>
+
+    <div className="FormTitle">
+        <NavLink to="/sign-in" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Sign In</NavLink> or <NavLink exact to="/" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Sign Up</NavLink>
+    </div>
+        <Route exact path="/" component={SignUpForm} ></Route>
+        <Route exact path="/sign-in" component={SignInForm} >
+        
+        </Route>
+      
+      </div>
+    </div>
+    </Router>
+  );
+}
+
+export default App;
